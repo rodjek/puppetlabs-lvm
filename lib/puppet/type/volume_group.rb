0988ea00568e423cbd6f8414821e1013396a0899
Tim Sharpe <tim@sharpe.id.au>
2013-04-07 13:35:52 -0700

    volume_group should autorequire physical_volumes

--- lib/puppet/type/volume_group.rb
+++ lib/puppet/type/volume_group.rb
@@ -1,15 +1,18 @@
 Puppet::Type.newtype(:volume_group) do
-    ensurable
+  ensurable
 
-    newparam(:name) do
-        desc "The name of the volume group."
-        isnamevar
-    end
+  newparam(:name) do
+    desc "The name of the volume group."
+    isnamevar
+  end
 
-    newproperty(:physical_volumes, :array_matching => :all) do
-        desc "The list of physical volumes to be included in the volume group; this
-             will automatically set these as dependencies, but they must be defined elsewhere
-             using the physical_volume resource type."
-    end
+  newproperty(:physical_volumes, :array_matching => :all) do
+    desc "The list of physical volumes to be included in the volume group; this
+         will automatically set these as dependencies, but they must be defined elsewhere
+         using the physical_volume resource type."
+  end
 
+  autorequire(:physical_volumes) do
+    [self[:physical_volumes]]
+  end
 end
